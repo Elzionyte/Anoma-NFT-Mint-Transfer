f9b44046a11c6ea815c81949cd4eea9965621b1b
Elzionyte <wasiuadam18@gmail.com>
2025-09-09 03:09:45 -0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>🚀 Anoma NFT Intent Demo</h1>
+      <p>This is your demo app running live on Vercel!</p>
+    </div>
+  );
+}
